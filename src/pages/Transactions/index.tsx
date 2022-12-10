18b72c6a55071c82e87ed081176c2d5a36f71167
Vinícius <viniscrv@gmail.com>
2022-12-10 02:24:57 -0300

    criado componente summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
 
 
@@ -6,6 +7,7 @@ export function Transactions() {
     return (
         <div>
             <Header/>
+            <Summary/>
         </div>
     )
 }
