790561373b0ac0a8eaead6801b5bb0a05b27063c
Vinícius <viniscrv@gmail.com>
2022-12-11 17:21:04 -0300

    adicionado cálculo de transações ao Summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,25 @@
 import { ArrowCircleUp, CurrencyDollar, ArrowCircleDown } from "phosphor-react";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { SummaryCard, SummaryContainer } from "./styles";
 
 
 export function Summary() {
+
+    const { transactions } = useContext(TransactionsContext);
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === "income") {
+            acc.income += transaction.price;
+            acc.total += transaction.price;
+        } else {
+            acc.outcome += transaction.price;
+            acc.total -= transaction.price;
+        }
+
+        return acc;
+    }, {income: 0, outcome: 0, total: 0});
+
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -11,7 +28,7 @@ export function Summary() {
                     <ArrowCircleUp size={32} color="#00b37e" />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>R$ {summary.income}</strong>
             </SummaryCard>
 
             <SummaryCard>
@@ -20,7 +37,7 @@ export function Summary() {
                     <ArrowCircleDown size={32} color="#f75a68" />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>R$ {summary.outcome}</strong>
             </SummaryCard>
 
             <SummaryCard variant="green">
@@ -29,7 +46,7 @@ export function Summary() {
                     <CurrencyDollar size={32} color="#fff" />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>R$ {summary.total}</strong>
             </SummaryCard>
         </SummaryContainer>
     );
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,31 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
 
-    interface Transaction {
-        id: number;
-        description: string;
-        type: "income" | "outcome";
-        price: number;
-        category: string;
-        createdAt: string;
-    }
-
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
-    
-    useEffect(() => {
-        async function loadTransactions() {
-            const response = await fetch("http://localhost:3000/transactions");
-            const data = await response.json();
-            setTransactions(data);
-        }
-
-        loadTransactions();
-    },[])
+    const { transactions } = useContext(TransactionsContext);
 
     return (
         <div>
